07ead7e44a02b9a9f98642e7e62d88935e906490
arch <arch@local>
2021-06-29 20:19:28 +0200

    update build script

--- build.bat
+++ build.bat
@@ -2,8 +2,7 @@
 rmdir /Q /S "build" 2>NUL
 rmdir /Q /S "dist/funscript-editor" 2>NUL
 del funscript-editor.spec 2>NUL
-pyinstaller --add-data="funscript_editor/config/*;funscript_editor/config" --hidden-import "pynput.keyboard._win32" --hidden-import "pynput.mouse._win32" funscript-editor.py
-xcopy /s "assets" "dist/funscript-editor"
+pyinstaller --add-data="funscript_editor/config/*;funscript_editor/config" --add-data="assets/*;." --hidden-import "pynput.keyboard._win32" --hidden-import "pynput.mouse._win32" funscript-editor.py
 copy /Y "funscript_editor\\VERSION.txt" "dist\\funscript-editor\\funscript_editor"
 powershell Compress-Archive -LiteralPath "dist/funscript-editor" -DestinationPath "dist/funscript-editor.zip"
 
