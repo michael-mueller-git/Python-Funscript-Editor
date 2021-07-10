ee5f95cf86419ca1b4f70b88404775ac3f6dbbce
arch <arch@local>
2021-07-10 19:30:59 +0200

    add linux build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+rm -rf build
+rm -rf dist/funscript-editor
+rm -f dist/funscript-editor.tar.gz
+rm -f funscript-editor.spec
+make docs
+
+pyinstaller --add-data="funscript_editor/config/*:funscript_editor/config/" --add-data="assets/*:./" --hidden-import=PyQt5.sip --hidden-import=sip --hidden-import "pynput.keyboard._xorg" --hidden-import "pynput.mouse._xorg" funscript-editor.py
+
+python_major_version="$(python3 --version | sed 's/\.*[0-9]*$//g' | awk '{print $2}' | tr -d '\n')"
+echo -e "\n >> Run post build for python ${python_major_version}"
+
+if [ -d $HOME/.local/lib/python${python_major_version}/site-packages/PyQt5 ]; then
+    cp -fv $HOME/.local/lib/python${python_major_version}/site-packages/PyQt5/sip.cpython-*.so ~/Repos/public/Python-Funscript-Editor/dist/funscript-editor/PyQt5
+else
+    echo "WARNING: PyQtt.sip not found"
+fi
+echo "done"
+
+echo "Add Version"
+git tag | sort | tail -n 1 > dist/funscript-editor/funscript_editor/VERSION.txt
+
+echo "Copy Application Documentation"
+mkdir -p dist/funscript-editor/funscript_editor/docs/
+cp -rf docs/app/site/ dist/funscript-editor/funscript_editor/docs/
+
+echo "Create Archive"
+tar -zcvf dist/funscript-editor.tar.gz dist/funscript-editor
