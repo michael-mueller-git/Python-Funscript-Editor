2c5a44fdc2f92f54d39c4b66a946fbdfbcaa93ed
arch <arch@local>
2021-08-29 16:37:49 +0200

    add OFS setup script for linux

---
+++ contrib/OpenFunscripter/openfunscripter_setup_linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -d ./OFS ]; then
+    echo "OpenFunscripter Source already downloaded"
+    pushd OFS
+    git pull
+else
+    git clone https://github.com/OpenFunscripter/OFS.git
+    pushd OFS
+    git submodule update --init
+    pushd lib/EASTL
+    git submodule update --init
+    popd
+    echo "OpenFunscripter Source downloaded to ./OFS"
+fi
+
+echo "build OFS"
+rm -rf build
+mkdir -p build
+pushd build
+cmake ..
+make -j$(expr $(nproc) \+ 1)
+popd
+popd
