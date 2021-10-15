96f72edb93b99f8baec4a4338779788b53b1b715
arch <arch@local>
2021-10-15 22:01:39 +0200

    improve win installler

--- contrib/windows_installer.py
+++ contrib/windows_installer.py
@@ -4,6 +4,7 @@ import re
 import platform
 import zipfile
 import os
+import sys
 
 from packaging import version
 from bs4 import BeautifulSoup # beautifulsoup4
@@ -18,10 +19,20 @@ class DownloadProgressBar(tqdm):
 
 
 def download_url(url, output_path):
-    with DownloadProgressBar(unit='B', unit_scale=True,
-                             miniters=1, desc=url.split('/')[-1]) as t:
+    with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:
         urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
 
+
+if platform.system() != "Windows":
+    print("ERROR: This installer only work on Windows")
+    sys.exit()
+
+ofs_extension_dir = os.path.expandvars(r'%APPDATA%\OFS_data\extensions')
+
+if not os.path.exists(ofs_extension_dir):
+    print("ERROR: OFS is not installed. Please download and install OFS. Befor running this installer open OFS once on your Computer")
+    print("Cancel installation")
+    sys.exit()
 
 release_url = "https://github.com/michael-mueller-git/Python-Funscript-Editor/releases"
 html_text = requests.get(release_url).text
@@ -30,7 +41,9 @@ download_urls = { version.parse(re.search(r'v[^/]*', x).group().lower().replace(
             if link.get('href').endswith(".zip") and "/releases/" in link.get('href')]
 }
 latest = max(download_urls)
-zip_file = "funscript-editor-v" +  str(latest) + ".zip"
+zip_file = os.path.join(ofs_extension_dir, "Funscript Generator Windows", "funscript-editor-v" +  str(latest) + ".zip")
+dest_dir = os.path.dirname(zip_file)
+os.makedirs(dest_dir, exist_ok = True)
 
 if not os.path.exists(zip_file):
     download_url(download_urls[latest], zip_file)
@@ -38,3 +51,5 @@ if not os.path.exists(zip_file):
 with zipfile.ZipFile(zip_file) as zf:
     for member in tqdm(zf.infolist(), desc='Extracting '):
         zf.extract(member, "funscript-editor")
+
+# TODO download the main.lua to the extension dir
