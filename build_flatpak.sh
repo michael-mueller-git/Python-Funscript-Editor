9b044dd71a91ee53c4c2e2fb050c3253b02d6454
arch <arch@local>
2021-11-06 13:39:08 +0100

    update build script

--- build_flatpak.sh
+++ build_flatpak.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+option="$1"
+[ -z "$option" ] && option="--default"
+
 sudo sysctl kernel.unprivileged_userns_clone=1
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install -y flathub org.kde.Platform//5.15 org.kde.Sdk//5.15
-
-
-option="$1"
-[ -z "$option" ] && option="none"
 
 if [ "$option" = "--install" ]; then
     flatpak-builder --user --install --force-clean build-dir org.flatpak.PythonFunscriptEditor.json
