0d2efd856b9f69af3ba877c31c4a5d61d5fb9aba
arch <arch@local>
2021-11-13 09:51:54 +0100

    add build and deploy script

---
+++ build_and_deploy.bat
@@ -0,0 +1,17 @@
+@echo off
+REM script to build an deploy ofs extension
+rmdir /Q /S "build" 2>NUL
+rmdir /Q /S "dist/funscript-editor" 2>NUL
+rmdir /Q /S "%APPDATA%\\OFS\\OFS_data\\extensions\\Funscript Generator Windows\\funscript-editor" 2>NUL
+del "dist\\funscript-editor.zip" 2>NUL
+del funscript-editor.spec 2>NUL
+del "%APPDATA%\\OFS\\OFS_data\\extensions\\Funscript Generator Windows\\main.lua" 2>NUL
+if not exist "%APPDATA%\\OFS\\OFS_data\\extensions\\Funscript Generator Windows" mkdir "%APPDATA%\\OFS\\OFS_data\\extensions\\Funscript Generator Windows"
+cd docs/app
+mkdocs build
+cd ../..
+pyinstaller --add-data="funscript_editor/config/*;funscript_editor/config/" --add-data="assets/*;./" --hidden-import "pynput.keyboard._win32" --hidden-import "pynput.mouse._win32" --noupx --icon=icon.ico funscript-editor.py
+move "docs\\app\\site" "dist\\funscript-editor\\funscript_editor\\docs"
+copy /Y "funscript_editor\\VERSION.txt" "dist\\funscript-editor\\funscript_editor"
+move "dist\\funscript-editor" "%APPDATA%\\OFS\\OFS_data\\extensions\\Funscript Generator Windows\\funscript-editor"
+copy /Y "contrib\\Installer\\assets\\main.lua" "%APPDATA%\\OFS\\OFS_data\\extensions\\Funscript Generator Windows"
