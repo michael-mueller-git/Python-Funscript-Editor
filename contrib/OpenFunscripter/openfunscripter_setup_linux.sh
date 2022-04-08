2e1955186221e2620cfc449c0d412f724ed3b8fa
arch <arch@local>
2022-04-08 21:56:56 +0200

    conda testing

--- contrib/OpenFunscripter/openfunscripter_setup_linux.sh
+++ contrib/OpenFunscripter/openfunscripter_setup_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if command -v apt; then
-    sudo apt install -y cmake build-essential libmpv-dev libglvnd-dev libxext-dev
+    sudo apt install -y cmake build-essential libmpv-dev libglvnd-dev libxext-dev make git gcc g++ cmake libmpv-dev libatlas-base-dev
 fi
 
 if [ -d ./OFS ]; then
@@ -28,5 +28,21 @@ make -j$(expr $(nproc) \+ 1)
 popd
 popd
 
-echo "copy lua plugin"
-cp -vf funscript_generator_linux.lua OFS/bin/data/lua
+echo "install ofs extension"
+mkdir -p ~/.local/share/OFS/OFS_data/extensions/MTFG
+pushd ~/.local/share/OFS/OFS_data/extensions/MTFG
+if [ -d ~/.local/share/OFS/OFS_data/extensions/MTFG/Python-Funscript-Editor ]; then
+    git clone https://github.com/michael-mueller-git/Python-Funscript-Editor.git
+fi
+
+pushd ~/.local/share/OFS/OFS_data/extensions/MTFG/Python-Funscript-Editor
+git pull
+source ~/anaconda3/etc/profile.d/conda.sh 2>/dev/null
+source ~/miniconda3/etc/profile.d/conda.sh 2>/dev/null
+conda env create -f environment_ubuntu.yaml
+bash download_ffmpeg.sh
+popd
+
+cp -fv ~/.local/share/OFS/OFS_data/extensions/MTFG/Python-Funscript-Editor/contrib/Installer/assets/main.lua ~/.local/share/OFS/OFS_data/extensions/MTFG/main.lua
+
+popd
