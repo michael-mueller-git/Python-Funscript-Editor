4e97eb3f13da8f7d904c76fc699f51db2eb13cff
arch <arch@local>
2022-06-08 19:27:28 +0200

    new attempt to fix loading screen

--- funscript_editor/ui/opencvui.py
+++ funscript_editor/ui/opencvui.py
@@ -363,6 +363,7 @@ class OpenCV_GUI(KeypressHandler):
         if self.preview_scaling_applied:
             return
 
+        self.preview_image_without_scale = copy.deepcopy(self.preview_image)
         self.preview_image = cv2.resize(
                 self.preview_image,
                 None,
@@ -406,8 +407,7 @@ class OpenCV_GUI(KeypressHandler):
             txt (str): text to display
         """
         if self.preview_image is not None:
-            self.set_background_image(np.full(self.preview_image.shape, 0, dtype=np.uint8))
-            self.preview_image = cv2.resize( self.preview_image, None, fx=1/self.monitor_preview_scaling, fy=1/self.monitor_preview_scaling)
+            self.set_background_image(np.full(self.preview_image_without_scale.shape, 0, dtype=np.uint8))
             self.print_text(txt, color=(0,0,255))
             self.show()
 
