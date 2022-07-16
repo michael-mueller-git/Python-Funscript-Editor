8ea6da5c73fe9b69561f7a58f8f81338ea5d1cf9
arch <arch@local>
2022-07-16 19:15:52 +0200

    improve install lua scripts

--- contrib/Installer/mtfg-ofs-extension-installer.py
+++ contrib/Installer/mtfg-ofs-extension-installer.py
@@ -87,12 +87,17 @@ def process_exists(process_name):
         return False
 
 
-def install_lua_scripts(src_dir, extension_dir):
-    for script in ["main.lua", "json.lua"]:
-        if os.path.exists(os.path.join(src_dir, "OFS", script)):
-            shutil.copy2(os.path.join(src_dir, "OFS", script), os.path.join(extension_dir, script))
-        else:
-            error("Installation failed (" + script + " missing)")
+def install_lua_scripts(root_src_dir, extension_dir):
+    for src_dir, _, files in os.walk(root_src_dir):
+        dst_dir = src_dir.replace(root_src_dir, extension_dir, 1)
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                os.remove(dst_file)
+            shutil.copy(src_file, dst_dir)
 
 
 def is_latest_version_installed(version_file, version):
