d70554b0173ea02d405b92b0a33b943d5c2f0aa9
arch <arch@local>
2022-12-27 14:18:05 +0100

    fix nix wrapper

--- nix_wrapper.sh
+++ nix_wrapper.sh
@@ -5,4 +5,4 @@ cmd="python3 `dirname $0`/funscript-editor.py $@"
 echo "cmd: $cmd" > /tmp/mtfg-nix.log
 echo "dir: $root_dir" >> /tmp/mtfg-nix.log
 # cd $root_dir && nix-shell --run "$cmd" >> /tmp/mtfg-nix.log 2>&1
-cd $root_dir && nix develop --command "$cmd" >> /tmp/mtfg-nix.log 2>&1
+cd $root_dir && nix develop --command $cmd >> /tmp/mtfg-nix.log 2>&1
