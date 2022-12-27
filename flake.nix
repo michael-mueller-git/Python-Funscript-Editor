a1770f7b6474e6841ff59efec43f416a05320762
arch <arch@local>
2022-12-27 14:08:28 +0100

    use flakes

--- flake.nix
+++ flake.nix
@@ -2,7 +2,14 @@
 {
   description = "MTFG";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs = {
+        url = "github:NixOS/nixpkgs/nixos-22.05";
+      };
+      flake-utils = {
+        url = "github:numtide/flake-utils";
+      };
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ let python =
     packageOverrides = self:
     super: {
       opencv4 = super.opencv4.overrideAttrs (old: rec {
-        buildInputs = old.buildInputs ++ [pkgs.qt5.qtbase pkgs.qt5.qtmultimedia pkgs.qt5.qtscript pkgs.qt5.wrapQtAppsHook];
+        buildInputs = old.buildInputs ++ [pkgs.qt5.full];
         cmakeFlags = old.cmakeFlags ++ ["-DWITH_QT=ON"];
       });
     };
@@ -14,9 +14,8 @@ in
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
       qt5.qtbase
+      qt5.full
       qt5.wrapQtAppsHook
-      libsForQt5.qt5.qtwayland
-      libsForQt5.qt5.qtx11extras
       python.pkgs.opencv4
       (python39.withPackages (p: with p; [
         coloredlogs
