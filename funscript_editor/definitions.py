4b8b78a14fb80ecfce2d8e75f492c87fc637799b
nix <nix@local>
2024-02-04 17:36:20 +0100

    add nix config support

--- funscript_editor/definitions.py
+++ funscript_editor/definitions.py
@@ -6,19 +6,29 @@ from pathlib import Path
 
 import multiprocessing as mp
 
-
-###############
-# DIRECTORIES #
-###############
-
+CONFIG_VERSION = 1
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_TEMPLATE_DIR = os.path.join(ROOT_DIR, 'config')
+
 if os.path.abspath(__file__).startswith("/nix"):
     CACHE_DIR = '/tmp/mtfg-cache'
+    CONFIG_DIR = os.path.join(os.path.join(os.path.expanduser('~'), '.config'), 'mtfg')
+    os.makedirs(CONFIG_DIR, exist_ok=True)
+    COPY_CONFIG_TEMPLATES = True
+    CONFIG_VERSION_FILE = os.path.join(CONFIG_DIR, ".version")
+    if os.path.exists(CONFIG_VERSION_FILE):
+        with open(CONFIG_VERSION_FILE, "r") as f:
+            if f.readlines() == [str(CONFIG_VERSION)]:
+                COPY_CONFIG_TEMPLATES = False
+    if COPY_CONFIG_TEMPLATES:
+        os.system(f"cp -rfv \"{CONFIG_TEMPLATE_DIR}/.\" \"{CONFIG_DIR}\"")
+        with open(CONFIG_VERSION_FILE, "w") as f:
+            f.write(str(CONFIG_VERSION))
 else:
     CACHE_DIR = os.path.join(ROOT_DIR, 'cache')
-CONFIG_DIR = os.path.join(ROOT_DIR, 'config')
-UI_DIR = os.path.join(ROOT_DIR, 'ui')
-ICON_PATH = os.path.join(CONFIG_DIR, 'icon.png')
+    CONFIG_DIR = CONFIG_TEMPLATE_DIR
+
+ICON_PATH = os.path.join(os.path.join(ROOT_DIR, 'config'), 'icon.png')
 DOCS_URL = "https://github.com/michael-mueller-git/Python-Funscript-Editor/tree/{tag}/docs/app/docs"
 RAW_TRACKING_DATA_CAHCE_FILE = os.path.join(CACHE_DIR, 'raw_tracking_data.json')
 
@@ -43,22 +53,11 @@ else:
     APP_DOCUMENTATION_DIR = ''
     CODE_DOCUMENTATION_DIR = ''
 
-
-################
-# CONFIG FILES #
-################
-
 UI_CONFIG_FILE = os.path.join(CONFIG_DIR, 'ui.yaml')
 WINDOWS_LOG_CONFIG_FILE = os.path.join(CONFIG_DIR, 'logging_windows.yaml')
 LINUX_LOG_CONFIG_FILE = os.path.join(CONFIG_DIR, 'logging_linux.yaml')
 HYPERPARAMETER_CONFIG_FILE = os.path.join(CONFIG_DIR, 'hyperparameter.yaml')
 SETTINGS_CONFIG_FILE = os.path.join(CONFIG_DIR, 'settings.yaml')
 PROJECTION_CONFIG_FILE = os.path.join(CONFIG_DIR, 'projection.yaml')
-
-
-##########
-# CONFIG #
-##########
-
 CPU_CORES = int(mp.cpu_count()-1)
 
